cbd27d5f1b518f4065225826e9245cb23f7f7390
TezBaydu <tezbaydu@gmail.com>
2021-12-18 22:06:50 +0000

    packages content case studies completed

--- packages/views.py
+++ packages/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from .models import Company, Project, Package
 
-# Create your views here.
+
+def all_projects(request):
+    """ A view to show Case Studies including filtering """
+
+    projects = Project.objects.all()
+    companies = None
+
+    if request.GET:
+        if 'company' in request.GET:
+            companies = request.GET['company']
+            projects = projects.filter(company__name=companies)
+            company = Company.objects.filter(name=companies)[0]
+
+    context = {
+        'projects': projects,
+        'company': company,
+    }
+
+    return render(request, 'packages/packages.html', context)
+
+
+def all_packages(request):
+    """ A view to show all packages """
+
+    packages = Package.objects.all()
+
+    context = {
+        'packages': packages,
+    }
+
+    return render(request, 'packages/packages.html', context)
