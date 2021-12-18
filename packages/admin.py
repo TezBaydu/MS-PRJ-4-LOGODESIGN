6932e1a86fc87fe37d6e98ad71ff1c1af53e0eb0
TezBaydu <tezbaydu@gmail.com>
2021-12-18 21:19:42 +0000

    packages admin customised

--- packages/admin.py
+++ packages/admin.py
@@ -1,7 +1,45 @@
 from django.contrib import admin
 from .models import Company, Project, Package
 
-# Register your models here.
-admin.site.register(Company)
-admin.site.register(Project)
-admin.site.register(Package)
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+        'description',
+        'logoimage',
+    )
+
+    ordering = ('name',)
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'company',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+class PackageAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'friendly_name',
+        'price',
+        'image',
+        'logo_count_request',
+        'quality_request',
+        'support_request',
+        'production_days',
+    )
+
+    ordering = ('sku',)
+
+
+admin.site.register(Company, CompanyAdmin)
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Package, PackageAdmin)
