d8100299888fc8657c6b3a0ed7d1fd431bb99e70
TezBaydu <tezbaydu@gmail.com>
2021-12-21 23:50:14 +0000

    added add to bag function but is in query

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,10 +1,24 @@
+from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from packages.models import Package
 
 
 def bag_contents(request):
 
     bag_items = []
     grand_total = 0
+    bag = request.session.get('bag', {})
+
+    for item_id in bag.items():
+        package = get_object_or_404(Package, pk=item_id)
+        bag_items.append({
+            'item_id': item_id,
+            'package': package,
+            'package.price': package.price,
+            'package.friendly_name': package.friendly_name,
+        })
+        grand_total += package.price
 
     context = {
         'bag_items': bag_items,
--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
-# from packages.models import Package
+from django.contrib import messages
+from packages.models import Package
 
 
 def view_bag(request):
@@ -11,18 +12,20 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ To add package to the bage """
 
-    # package = get_object_or_404(Package, pk=item_id)
+    package = get_object_or_404(Package, pk=item_id)
     # package_add = request.POST.get('package_add')
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if not bag.keys():
         bag[item_id] = {}
+        messages.success(
+            request, f'Added {package.friendly_name} Logo Package to your bag')
     else:
-        # messages.error(
-        #     request, "You already have an item in the bag and can only order one \
-        #          at a time. Please edit your bag or if details are good,\
-        #               proceed to purchase. ")
+        messages.error(
+            request, "You already have an item in the bag and can only order one \
+                 at a time. Please edit your bag or if details are good,\
+                      proceed to purchase. ")
         return redirect('view_bag')
 
     request.session['bag'] = bag
